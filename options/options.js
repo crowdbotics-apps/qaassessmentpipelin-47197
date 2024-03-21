432aacce70cf2264d8544fb319efdd38f507869b
Crowdbotics <team@crowdbotics.com>
2024-03-21 19:52:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "qaassessmentpipelin-47197",
+  url: "https://qaassessmentpipelin-47197.botics.co",
+  api: "https://qaassessmentpipelin-47197.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
